6b9026758414bdcf49d180ad6926c37c567c9aa3
Abhay-Pratap99 <abhay1065pratap@gmail.com>
2026-09-05 03:18:32 +0530

    ol

---
+++ objectLiterels.js
@@ -0,0 +1,11 @@
+const student={
+    name:"Abhay",
+    RollNo:34,
+    branch:"CSE"
+}
+console.log(student.RollNo);
+student.RollNo=4;
+console.log(student.RollNo);
+let l="Abhay";
+let j=l.toUpperCase().length; // Method chaining Concept
+console.log(j);
