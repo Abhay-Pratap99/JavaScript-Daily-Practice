cdc624e7e06430e44cf76e0679e39f055d6edbd9
Abhay-Pratap99 <abhay1065pratap@gmail.com>
2026-09-05 01:18:11 +0530

    Array operation

--- Array.js
+++ Array.js
@@ -4,3 +4,29 @@ let arr=["Apple","Mango",2.4,5,3];
 for(let i=0;i<arr.length;i++){
     console.log(`Index ${i} Element is ${arr[i]}`);
 }
+
+//Apply basic property like push pop shift and unshfit
+arr.push(49);
+arr.push(44);
+for(let i=0;i<arr.length;i++){
+console.log(`After push operation${arr[i]}`);
+}
+
+//Shift Operation
+arr.shift(45);
+arr.shift(9.3);
+for(let i=0;i<arr.length;i++){
+    console.log(`Shift operation${arr[i]}`);
+}
+
+//Unshift Operation
+
+arr.unshift(45);
+arr.unshift(33333);
+for(let i=0;i<arr.length;i++){
+    console.log(`Unsift Operation${arr[i]}`);
+}
+
+for(let i=0;i<arr.length;i++){
+console.log(arr[i]);
+}
